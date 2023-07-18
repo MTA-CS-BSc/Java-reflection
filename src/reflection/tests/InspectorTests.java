8afd310fb8e3d3c8b58341110cf0a365c7c3526a
Maya Raskin <mayaraskin14@gmail.com>
2023-07-18 15:37:02 +0300

    Implemented a test for methods amount

--- src/reflection/tests/InspectorTests.java
+++ src/reflection/tests/InspectorTests.java
@@ -1,18 +1,31 @@
 package reflection.tests;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reflection.api.Inspector;
 import reflection.classes.Rectangle;
 
 public class InspectorTests {
+    Rectangle testRectangle;
+    Inspector inspector;
+
+    public InspectorTests() {
+        testRectangle = new Rectangle(1, 1);
+        inspector = new Inspector();
+        inspector.load(testRectangle);
+    }
 
     @Test
+    @DisplayName("Test for ctors amount")
     public void testGetTotalNumberOfCtors() {
-        Rectangle testRectangle = new Rectangle(1, 1);
-        Inspector inspector = new Inspector();
-
-        inspector.load(testRectangle);
         Assertions.assertEquals(2, inspector.getTotalNumberOfConstructors());
     }
+
+    @Test
+    @DisplayName("Test for methods amount")
+    public void testGetTotalNumberOfMethod() {
+        Assertions.assertEquals(6, inspector.getTotalNumberOfMethods());
+    }
+
 
 }
